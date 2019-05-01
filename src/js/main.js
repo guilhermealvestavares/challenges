15d1d9d8308b0c257029f565ab49900d1d77ebe7
Guilherme <guilhermetavalves@gmail.com>
2019-05-01 01:39:13 -0300

    adicionando feature de imagem lida da API

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 var textDescription = document.querySelector(".photoBlock__description");
 var breed = document.querySelector(".inputBlock__breeds");
+var photoDog = document.querySelector(".photoBlock__photo");
+
 
 window.onload = getBreed();
 
 $(".inputBlock__dog--button").click(function() {
 colorSet();
 fontSet();
-getBreed();
 namePetSet();
-validaCampos();
+getBreedPhoto();
 });
 
 function colorSet(){
@@ -25,25 +26,45 @@ function namePetSet(){
     document.querySelector(".photoBlock__description").innerHTML =nomePet.value;
 }
 
-
-
 function getBreed(){
-let urlStr = "https://dog.ceo/api/breeds/list/all";
-        $.ajax({
-            url : urlStr,
-            type : "get",
-            dataType : "json",
-            success : function(data){
-                for (let i = 0; i < Object.entries(data.message).length; i++){
-                    newOption = document.createElement("option"); 
-                    newOption.value =Object.entries(data.message)[i][0];
-                    newOption.text=Object.entries(data.message)[i][0];
-                    breed.add(newOption);
-                }
-            },
-            error : function(erro){
-                console.log(erro);
+    let urlStr = "https://dog.ceo/api/breeds/list/all";
+    $.ajax({
+        url : urlStr,
+        type : "get",
+        dataType : "json",
+        success : function(data){
+            for (let i = 0; i < Object.entries(data.message).length; i++){
+                newOption = document.createElement("option"); 
+                newOption.value =Object.entries(data.message)[i][0];
+                newOption.text=Object.entries(data.message)[i][0];
+                breed.add(newOption);
             }
-        });
-    }  
+        },
+        error : function(erro){
+            console.log(erro);
+        }
+    });
+}  
+
+function getBreedPhoto(){
+    let urlStrPhotos = "https://dog.ceo/api/breed/"+breed.value+"/images";
+    $.ajax({
+        url : urlStrPhotos,
+        type : "get",
+        dataType : "json",
+        success : function(dataPhoto){
+            let numberImages = (Object.entries(dataPhoto.message).length);
+            let numberImagesRandom = (Math.round(Math.random() * (numberImages- 0 + 1) + 0));
+            photoDog.src = Object.entries(dataPhoto.message)[numberImagesRandom][1];
+
+        },
+        error : function(erro){
+            console.log(erro);
+        }
+    });
+}  
     
+
+
+    
+
