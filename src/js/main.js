e8231d16ce4c61231e0c5ed0d6367272d141f6e4
Guilherme <guilhermetavalves@gmail.com>
2019-05-02 00:30:04 -0300

    ajuste em localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,26 @@ var breed = document.querySelector(".inputBlock__breeds");
 var photoDog = document.querySelector(".photoBlock__photo");
 
 
-window.onload = getBreed();
+window.onload = init();
+
+function init() {
+    getBreed();
+    saveInfosBreed();
+    getInfosBreed();
+    saveInfosFont();
+    getInfosFont();
+    saveInfosName();
+    getInfosName();
+    getImage();
+   
+}
 
 $(".inputBlock__dog--button").click(function() {
 getBreedPhoto();
 colorSet();
 fontSet();
 namePetSet();
+saveImage();
 });
 
 function colorSet(){
@@ -26,6 +39,7 @@ function namePetSet(){
     document.querySelector(".photoBlock__description").innerHTML =nomePet.value;
 }
 
+//função para consumir raça e popular o select
 function getBreed(){
     let urlStr = "https://dog.ceo/api/breeds/list/all";
     $.ajax({
@@ -44,8 +58,10 @@ function getBreed(){
             console.log(erro);
         }
     });
-}  
+}
 
+
+//função para consumir imagem
 function getBreedPhoto(){
     let urlStrPhotos = "https://dog.ceo/api/breed/"+breed.value+"/images";
     $.ajax({
@@ -57,28 +73,76 @@ function getBreedPhoto(){
             let numberImagesRandom = (Math.round(Math.random() * (numberImages- 0 + 1) + 0));
             photoDog.src = Object.entries(dataPhoto.message)[numberImagesRandom][1];
 
-            //LOCALSTORAGE
-            $('.inputBlock__breeds').on('change', () => {
-                localStorage.setItem('key', breed.val);
-            })  
-            if(localStorage.getItem('key')){
-                $('.inputBlock__breeds').text(localStorage.getItem(key));
-            }else{
-                console.log("não existe")
-            }
-           
-
         },
         error : function(erro){
             console.log(erro);
         }
     });
-}  
+}
+
+//salvando opções de raça no localstorage
+function saveInfosBreed() {
+    $('.inputBlock__breeds').change(function() {
+        localStorage.setItem('breed', this.value);
+    })
+}
+function getInfosBreed() {
+    $(document).ajaxComplete(function (event, xhr, settings) {
+        if (settings.url === "https://dog.ceo/api/breeds/list/all") {
+            if (localStorage.getItem('breed')) {
+                $('.inputBlock__breeds').val(localStorage.getItem('breed'));
+            }
+        }
+    });
+}
+
+//salvando opções de cor no localstorage
+function saveInfosColor() {
+    $('.inputBlock__colors').change(function() {
+        localStorage.setItem('color', this.value);
+    })
+}
+function getInfosColor() {
+    if (localStorage.getItem('color')) {
+        $('.inputBlock__colors').val(localStorage.getItem('color'));
+    }
+}
 
 
+//salvando opções de fonte no localstorage
+function saveInfosFont() {
+    $('.inputBlock__fonts').change(function() {
+        localStorage.setItem('font', this.value);
+    })
+}
+function getInfosFont() {
+    if (localStorage.getItem('font')) {
+        $('.inputBlock__fonts').val(localStorage.getItem('font'));
+    }
+}
 
-    
+//salvando opções de nome no localstorage
+function saveInfosName() {
+    $('.photoBlock__description').on(function() {
+        localStorage.setItem('name', this.value);
+    })
+}
+function getInfosName() {
+    if (localStorage.getItem('name')) {
+        $('.photoBlock__description').val(localStorage.getItem('name'));
+    }
+}
 
-
-    
-
+//salvando foto no localstorage
+function saveImage() {
+    $(document).ajaxComplete(function (event, xhr, settings) {
+        if (settings.url.indexOf('/images') >= 0) {
+            localStorage.setItem('image', $('.photoBlock__photo').attr('src'))
+        }
+    })
+}
+function getImage() {
+    if (localStorage.getItem('image')) {
+        $('.photoBlock__photo').attr('src', localStorage.getItem('image'));
+    }
+}
